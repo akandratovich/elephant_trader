1020bc37f61115ed64a770978b5382e5b38d585e
Andrew Kondratovich <andrew.kondratovich@gmail.com>
2011-09-14 18:07:37 +0300

    twitter bot

---
+++ caller.py
@@ -0,0 +1,86 @@
+﻿# -*- coding: utf-8 -*-
+import oauth, tweepy
+import time, sqlite3
+import logging
+
+def init_api():
+	consumer_key = ""
+	consumer_secret = ""
+	access_key=""
+	access_secret=""
+	proxy_h = ""
+	proxy_p = ""
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_key, access_secret)
+	return tweepy.API(auth, proxy_host = proxy_h, proxy_port = proxy_p)
+
+def init_db():
+	path = "pref.db"
+	conn = sqlite3.connect(path)
+	try:
+		conn.execute('select * from called')
+	except sqlite3.OperationalError, e:
+		conn.execute('create table called (id int)')
+		conn.commit()
+	return conn
+
+def init_facts():
+	facts = set()
+	path = "facts.txt"
+	with open(path) as f:
+		for line in f:
+			facts.add(line.strip())
+	f.close()
+	return facts
+
+def save_id(conn, id):
+	conn.execute('insert into called values(' + str(id) + ')')
+	conn.commit()
+
+def is_called(conn, id):
+	(count,) = conn.execute('select count(id) from called where id = ' + str(id)).fetchone()
+	return count > 0
+
+def get_random_fact(facts):
+	f = facts.pop()
+	facts.add(f)
+	return f
+
+def format_message(mention, facts):
+	user = '@' + mention.from_user
+	fact = get_random_fact(facts)
+	message = user + unicode(" а Вы знали, что " + fact, "utf-8") + " ?"
+	return message
+
+def work(api, conn, facts):
+	update_time = 120
+	while True:
+		try:
+			m = api.search(unicode("слон", "utf-8"))
+			for i in m:
+				if not is_called(conn, i.from_user_id):
+					try:
+						save_id(conn, i.from_user_id)
+						api.update_status(status = format_message(i, facts), in_reply_to_status_id = i.id)
+						logging.info('send for @' + i.from_user + ': ' + str(i.id))
+					except tweepy.error.TweepError, e:
+						logging.info('publish error for @' + i.from_user + ' - ' + str(i.id) + ': ' + str(e))
+		except tweepy.error.TweepError, e:
+			logging.info('cannot get mentions: ' + str(e))
+		time.sleep(update_time)
+
+if __name__ == '__main__':
+	logging.basicConfig(filename = 'caller.log',
+						level = logging.INFO,
+						format = '%(asctime)s %(levelname)-8s %(message)s',
+						datefmt = '%d.%m.%Y %H:%M:%S')
+	api = init_api()
+	conn = init_db()
+	facts = init_facts()
+	try:
+		work(api, conn, facts)
+	except KeyboardInterrupt, e:
+		logging.info('interrupted')
+	finally:
+		conn.close()
+	exit()
---
+++ replier.py
@@ -0,0 +1,90 @@
+﻿# -*- coding: utf-8 -*-
+import oauth, tweepy
+import time, sqlite3
+import logging
+
+def init_api():
+	consumer_key = ""
+	consumer_secret = ""
+	access_key=""
+	access_secret=""
+	proxy_h = ""
+	proxy_p = ""
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_key, access_secret)
+	return tweepy.API(auth, proxy_host = proxy_h, proxy_port = proxy_p)
+
+def init_db():
+	path = "pref.db"
+	conn = sqlite3.connect(path)
+	try:
+		conn.execute('select * from replied')
+	except sqlite3.OperationalError, e:
+		conn.execute('create table replied (id int)')
+		conn.commit()
+	return conn
+
+def get_since(conn):
+	(mx,) = conn.execute('select max(id) from replied').fetchone()
+	return mx
+
+def save_id(conn, id):
+	conn.execute('insert into replied values(' + str(id) + ')')
+	conn.commit()
+
+def is_replied(conn, id):
+	(count,) = conn.execute('select count(id) from replied where id = ' + str(id)).fetchone()
+	return count > 0
+
+def format_message(mention):
+	user = '@' + mention.author.screen_name
+	text = mention.text.strip()
+	if text.find('@elephant_trader') == 0:
+		text = text[16:].strip()
+	maxl = 140 - 45 - len(user)
+	if len(text) > (maxl):
+		text = text[0:maxl-3] + '...'
+	message = user + unicode(" все говорят «", "utf-8") + text + unicode("». А ты возьми, и купи слона!", "utf-8")
+	return message
+
+def can_tweet(conn, mention):
+	ex = not is_replied(conn, mention.id)
+	me = not (mention.author.id == 372156581)
+	rt = not mention.retweeted
+	return ex and me and rt
+
+def work(api, conn):
+	update_time = 60
+	while True:
+		try:
+			since = get_since(conn)
+			if since:
+				m = api.mentions(since_id = since)
+			else:
+				m = api.mentions()
+			for i in m:
+				if can_tweet(conn, i):
+					try:
+						save_id(conn, i.id)
+						api.update_status(status = format_message(i), in_reply_to_status_id = i.id)
+						logging.info('send for @' + i.author.screen_name + ': ' + str(i.id))
+					except tweepy.error.TweepError, e:
+						logging.info('publish error for @' + i.author.screen_name + ' - ' + str(i.id) + ': ' + str(e))
+		except tweepy.error.TweepError, e:
+			logging.info('cannot get mentions: ' + str(e))
+		time.sleep(update_time)
+
+if __name__ == '__main__':
+	logging.basicConfig(filename = 'replier.log',
+						level = logging.INFO,
+						format = '%(asctime)s %(levelname)-8s %(message)s',
+						datefmt = '%d.%m.%Y %H:%M:%S')
+	api = init_api()
+	conn = init_db()
+	try:
+		work(api, conn)
+	except KeyboardInterrupt, e:
+		logging.info('interrupted')
+	finally:
+		conn.close()
+	exit()
